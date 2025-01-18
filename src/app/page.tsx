eb75b13d5d908f14a755237fd5a151bb8b52f509
Ray Zhou <rayzhou4@gmail.com>
2025-01-18 14:43:35 -0800

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div>Thhis is the home page for swiping</div>;
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-[#FFFFFF]">
+      <div className="flex space-x-4">
+        <button className="w-48 px-6 py-3 bg-[#FAFAFA] text-[#171717] rounded-lg hover:bg-blue-700 transition-colors duration-200">
+          Get Started
+        </button>
+        <button className="w-48 px-6 py-3 bg-gray-600 text-white rounded-lg hover:bg-gray-700 transition-colors duration-200">
+          Learn More
+        </button>
+      </div>
+    </main>
+  );
 }
