9034ed7797030a68f3650432cb8fed5bf173d9bc
Ray Zhou <rayzhou4@gmail.com>
2025-01-18 17:07:14 -0800

    -added fonts -landing page set up -added bg and logo pics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-[#FFFFFF]">
-      <div className="flex space-x-4">
-        <button className="w-48 px-6 py-3 bg-[#FAFAFA] text-[#171717] rounded-lg hover:bg-blue-700 transition-colors duration-200">
-          Get Started
-        </button>
-        <button className="w-48 px-6 py-3 bg-gray-600 text-white rounded-lg hover:bg-gray-700 transition-colors duration-200">
-          Learn More
-        </button>
+    <main className="min-h-screen flex flex-col items-center justify-center bg-[url('../pictures/bg.png')] bg-cover bg-center bg-landingpage bg-no-repeat h-screen w-screen">
+      <div className="flex flex-col items-center justify-center -mt-20">
+        <p className="text-center text-[#171717] mb-5" style={{ fontFamily: 'PPObjectSans-Heavy', fontSize: '16px', fontWeight: 1000, letterSpacing: '0.06em' }}>
+          Where every stay creates lifelong connections
+        </p>
+        <div className="flex space-x-16">
+          <button className="w-42 px-14 py-1 bg-[#FFFFFF] text-[#171717] border-[#171717] rounded-full hover:bg-[#FAFAFA] transition-colors duration-200" style={{ fontFamily: 'PPObjectSans-Regular', fontSize: '10px', borderWidth: '1px' }}>
+            Sign up
+          </button>
+          <button className="w-42 px-14 py-1 bg-[#FFFFFF] text-[#171717] border-[#171717] rounded-full hover:bg-[#FAFAFA] transition-colors duration-200" style={{ fontFamily: 'PPObjectSans-Regular', fontSize: '10px', borderWidth: '1px' }}>
+            Login
+          </button>
+        </div>
       </div>
     </main>
   );
